29ada8c178921bf21042aa2276cb1f7c35ad4c00
Bao Ho <bao.ho@tookitaki.com>
2020-03-17 06:57:27 +0800

    [Hook] - finished useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import ClassCounter from "./components/ClassCounter";
-import HookCounter from "./components/HookCounter";
-import HookCounterTwo from "./components/HookCounterTwo";
-import HookCounterThree from "./components/HookCounterThree";
-import HookCounterFour from "./components/HookCounterFour";
+import ClassCounter from "./components/useState/ClassCounter";
+import HookCounter from "./components/useState/HookCounter";
+import HookCounterTwo from "./components/useState/HookCounterTwo";
+import HookCounterThree from "./components/useState/HookCounterThree";
+import HookCounterFour from "./components/useState/HookCounterFour";
 function App() {
   return (
     <div className="App">
