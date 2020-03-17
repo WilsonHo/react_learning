0a2350f9a3d375a7ba905225aa7eca20713eeba8
Bao Ho <bao.ho@tookitaki.com>
2020-03-17 22:43:58 +0700

    [Hook] - useContext part 02 finish

--- src/components/useContext/ComponentE.js
+++ src/components/useContext/ComponentE.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import ComponentF from "./ComponentF";
+import {UserContext} from "../../App";
 
 const ComponentE = pros => {
+  const  user = useContext(UserContext);
   return (
     <div>
-      <ComponentF/>
+      {user}
+      {/*<ComponentF/>*/}
     </div>
   );
 };
